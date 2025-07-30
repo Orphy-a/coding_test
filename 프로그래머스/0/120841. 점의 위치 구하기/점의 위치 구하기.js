f58179fede2c32545534d19ce1043c806ff98af5
Orphy-a <jaeyoung0560@gmail.com>
2025-07-30 16:44:04 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,18 @@
+function solution(dot) {
+    
+    let result = 0;
+    
+    if(dot[0]>0 && dot[1]>0){
+        result = 1;
+        return result;
+    }else if(dot[0]<0 && dot[1]>0){
+        result = 2;
+        return result;
+    }else if(dot[0]<0 && dot[1]<0){
+        result = 3;
+        return result;
+    }else if(dot[0]>0 && dot[1]<0){
+        result = 4;
+        return result;
+    }
+}
