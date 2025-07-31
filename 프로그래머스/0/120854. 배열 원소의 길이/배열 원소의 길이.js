14adae60f12d4dbb59ea9a1509703dda6b67621c
Orphy-a <jaeyoung0560@gmail.com>
2025-07-31 17:32:42 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,12 @@
+function solution(strlist) {
+    
+    const result = [];
+    
+    for(let i = 0;i<strlist.length;i++){
+        result[i] = strlist[i].length;
+    }
+    
+
+    return result;
+    
+}
