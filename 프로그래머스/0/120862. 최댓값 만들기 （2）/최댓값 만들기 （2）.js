f5e6473c997e51eae989fd13f7fad8f69a336319
Orphy-a <jaeyoung0560@gmail.com>
2025-08-27 14:24:36 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    numbers.sort((a,b) => a-b);
+    
+    const max = Math.max(
+    numbers[0] * numbers[1],
+    numbers[numbers.length -1] * numbers[numbers.length -2]);
+    return max
+}
