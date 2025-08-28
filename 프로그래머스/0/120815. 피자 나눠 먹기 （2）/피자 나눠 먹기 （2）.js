65342110924e8bac584161098e89c0450036573b
Orphy-a <jaeyoung0560@gmail.com>
2025-08-28 15:53:25 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,16 +1,14 @@
-function gcd(a, b) {
-  while (b !== 0) {
-    let temp = a % b;
-    a = b;
-    b = temp;
-  }
-  return a;
-}
-
-function lcm(a, b) {
-  return (a * b) / gcd(a, b);
-}
-
 function solution(n) {
-  return lcm(n, 6) / 6;
+    
+    let piece = 6;
+    
+    while(true){
+        if(piece % n === 0){
+            break;
+        }
+        piece += 6
+    }
+    return piece / 6
+    
+    
 }
